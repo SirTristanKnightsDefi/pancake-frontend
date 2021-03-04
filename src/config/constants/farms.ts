0f5dd5d2734c5ca35a56f307166a44382f36b10a
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-04 11:32:46 -0500

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -19,6 +19,21 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 1,
+    lpSymbol: 'KNIGHT-BNB LP',
+    lpAddresses: {
+      97: '0x85CAf926427B9B930Bc858ADD1CCC15cE1581Db0', // KNIGHT-BNB LP
+      56: '0x85CAf926427B9B930Bc858ADD1CCC15cE1581Db0', // KNIGHT-BNB LP
+    },
+    tokenSymbol: 'KNIGHT',
+    tokenAddresses: {
+      97: '0xa828ED8000B0B7e881DAC3922D4c9bB1b0BA92a9', // KNIGHT
+      56: '0xa828ED8000B0B7e881DAC3922D4c9bB1b0BA92a9', // KNIGHT
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 2,
     lpSymbol: 'TABLE-BNB LP',
     lpAddresses: {
       97: '0x3468eac4eFB3e1FCeB3229Bc86F35da7749b0bCa', // TABLE-BNB LP
@@ -33,7 +48,7 @@ const farms: FarmConfig[] = [
     quoteTokenAdresses: contracts.wbnb,
   },
   {
-    pid: 2,
+    pid: 3,
     lpSymbol: 'KNIGHT-BUSD LP',
     lpAddresses: {
       97: '0xe6437ae391b0437BA349BB25160E2Cb8D602C358', // KNIGHT-BUSD LP
@@ -58,36 +73,6 @@ const farms: FarmConfig[] = [
     tokenAddresses: {
       97: '0xe9e7cea3dedca5984780bafc599bd69add087d56', // BUSD
       56: '0xe9e7cea3dedca5984780bafc599bd69add087d56', // BUSD
-    },
-    quoteTokenSymbol: QuoteToken.BNB,
-    quoteTokenAdresses: contracts.wbnb,
-  },
-  {
-    pid: 14,
-    lpSymbol: 'ETH-BNB LP',
-    lpAddresses: {
-      97: '0xE66790075ad839978fEBa15D4d8bB2b415556a1D',
-      56: '0x70D8929d04b60Af4fb9B58713eBcf18765aDE422',
-    },
-    tokenSymbol: 'ETH',
-    tokenAddresses: {
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
-      56: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
-    },
-    quoteTokenSymbol: QuoteToken.BNB,
-    quoteTokenAdresses: contracts.wbnb,
-  },
-  {
-    pid: 3,
-    lpSymbol: 'ETH-BNB LP',
-    lpAddresses: {
-      97: '0xE66790075ad839978fEBa15D4d8bB2b415556a1D',
-      56: '0x70D8929d04b60Af4fb9B58713eBcf18765aDE422',
-    },
-    tokenSymbol: 'ETH',
-    tokenAddresses: {
-      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
-      56: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
     },
     quoteTokenSymbol: QuoteToken.BNB,
     quoteTokenAdresses: contracts.wbnb,
