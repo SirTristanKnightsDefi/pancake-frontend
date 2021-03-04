9a37417f5fb9002633fd5c24e919bdf6425bb3a9
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-04 10:50:42 -0500

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -19,12 +19,12 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 1,
-    lpSymbol: 'TABLE-BNB LP',
+    lpSymbol: 'KNIGHT-BNB LP',
     lpAddresses: {
-      97: '0x3468eac4eFB3e1FCeB3229Bc86F35da7749b0bCa', // TABLE-BNB LP
-      56: '0x3468eac4eFB3e1FCeB3229Bc86F35da7749b0bCa', // TABLE-BNB LP
+      97: '0x85CAf926427B9B930Bc858ADD1CCC15cE1581Db0', // TABLE-BNB LP
+      56: '0x85CAf926427B9B930Bc858ADD1CCC15cE1581Db0', // TABLE-BNB LP
     },
-    tokenSymbol: 'TABLE',
+    tokenSymbol: 'KNIGHT',
     tokenAddresses: {
       97: '0xa828ED8000B0B7e881DAC3922D4c9bB1b0BA92a9', // KNIGHT
       56: '0xa828ED8000B0B7e881DAC3922D4c9bB1b0BA92a9', // KNIGHT
@@ -64,6 +64,21 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 14,
+    lpSymbol: 'ETH-BNB LP',
+    lpAddresses: {
+      97: '0xE66790075ad839978fEBa15D4d8bB2b415556a1D',
+      56: '0x70D8929d04b60Af4fb9B58713eBcf18765aDE422',
+    },
+    tokenSymbol: 'ETH',
+    tokenAddresses: {
+      97: '0xE02dF9e3e622DeBdD69fb838bB799E3F168902c5',
+      56: '0x2170ed0880ac9a755fd29b2688956bd959f933f8',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 3,
     lpSymbol: 'ETH-BNB LP',
     lpAddresses: {
       97: '0xE66790075ad839978fEBa15D4d8bB2b415556a1D',
