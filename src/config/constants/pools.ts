7bd0cd591b2cc12033cb43c42843515dfe88cbca
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-04 22:41:56 -0500

    Updated to Production Contracts

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -5,10 +5,10 @@ const pools: PoolConfig[] = [
     sousId: 0,
     tokenName: 'KNIGHT',
     stakingTokenName: QuoteToken.KNIGHT,
-    stakingTokenAddress: '0xC36649fF473e0a558eC50b0F8ce650940B9BA701', // KNIGHT
+    stakingTokenAddress: '0x16C0e0936E1B38Ff1F9b8a1e75d8ba29aDf87d30', // KNIGHT
     contractAddress: {
-      97: '0xdf79B790f18CCe224d4d2DB7Fa6835c50BCF2C89', // MasterChef
-      56: '0xdf79B790f18CCe224d4d2DB7Fa6835c50BCF2C89', // MasterChef
+      97: '0x0E9b7E13a543b7F1651A6BB03a47900515eBceCf', // MasterChef
+      56: '0x0E9b7E13a543b7F1651A6BB03a47900515eBceCf', // MasterChef
     },
     poolCategory: PoolCategory.CORE,
     projectLink: 'https://pancakeswap.finance',
