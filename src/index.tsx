bf4ee4ff3a9d922790010db8e84c3d87642bce26
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-06 11:41:21 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import Providers from './Providers'
+import './web.config'
 
 ReactDOM.render(
   <React.StrictMode>
