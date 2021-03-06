67027da92223c274e26381de6a4af3f493572091
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-06 14:44:18 -0500

    Update copyIISConfig.js

--- iisConfig/copyIISConfig.js
+++ iisConfig/copyIISConfig.js
@@ -1,6 +1,6 @@
 const fs = require('fs-extra');
 
-const webConfigPath = './build/web.config';
+const webConfigPath = './dist/web.config';
 
 if (fs.existsSync(webConfigPath)) {
     fs.unlinkSync(webConfigPath);
