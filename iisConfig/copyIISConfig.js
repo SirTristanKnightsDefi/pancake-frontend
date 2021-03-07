66e3fd52c19f8f88ded5378443749b47a4ebc55a
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-06 19:37:37 -0500

    Try again... Add staticwebapp.config.json.

--- iisConfig/copyIISConfig.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-const webConfigPath = './dist/web.config';
-
-if (fs.existsSync(webConfigPath)) {
-    fs.unlinkSync(webConfigPath);
-}
-
-fs.copySync('./iisConfig/web.config', webConfigPath);
