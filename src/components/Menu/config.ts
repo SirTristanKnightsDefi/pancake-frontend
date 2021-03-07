35e24f89035e1c6be36eb112d9285677c3b2b5ac
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-07 12:39:17 -0500

    Update config.ts
    Added Buy links in menu.

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -29,8 +29,16 @@ const config: MenuEntry[] = [
         href: 'https://bscscan.com/address/0x16c0e0936e1b38ff1f9b8a1e75d8ba29adf87d30',
       },
       {
+        label: 'Buy KNIGHT',
+        href: 'https://exchange.pancakeswap.finance/#/swap?inputCurrency=0x16C0e0936E1B38Ff1F9b8a1e75d8ba29aDf87d30',
+      },
+      {
         label: 'TABLE Contract',
         href: 'https://bscscan.com/address/0xf5a2f7418035ce76967f515f39d65719bb0453b6',
+      },
+      {
+        label: 'Buy TABLE',
+        href: 'https://exchange.pancakeswap.finance/#/swap?inputCurrency=0xf5a2f7418035ce76967f515f39d65719bb0453b6',
       },
     ],
   },
