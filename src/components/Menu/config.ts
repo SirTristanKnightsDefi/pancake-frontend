134bfd943d64deda30b36aef7a711bffc16fdfb0
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-08 08:16:10 -0500

    Updates to menu links.

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -17,20 +17,36 @@ const config: MenuEntry[] = [
     href: '/pools',
   },
   {
-    label: 'Info',
+    label: 'Blog',
+    icon: 'MoreIcon',
+    href: 'https://knightsoftheroundtable-bsc.medium.com/',
+  },
+  {
+    label: 'KNIGHT Info',
     icon: 'MoreIcon',
     items: [
-      {
-        label: 'Blog',
-        href: 'https://knightsoftheroundtable-bsc.medium.com/',
-      },
+      
       {
         label: 'KNIGHT Contract',
         href: 'https://bscscan.com/address/0x16c0e0936e1b38ff1f9b8a1e75d8ba29adf87d30',
       },
       {
+        label: 'Buy KNIGHT',
+        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x16C0e0936E1B38Ff1F9b8a1e75d8ba29aDf87d30',
+      },
+    ]
+  },
+  {
+    label: 'TABLE Info',
+    icon: 'MoreIcon',
+    items: [
+      {
         label: 'TABLE Contract',
         href: 'https://bscscan.com/address/0xf5a2f7418035ce76967f515f39d65719bb0453b6',
+      },
+      {
+        label: 'Buy TABLE',
+        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xf5a2f7418035ce76967f515f39d65719bb0453b6',
       },
     ],
   },
