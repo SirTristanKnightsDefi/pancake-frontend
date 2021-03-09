0f7593af4e6d27bfd47575863067e1efcf6b58ee
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-08 20:26:15 -0500

    Update Yarn lock again. and fix icon for honor

--- src/views/Pools/components/CardFooter.tsx
+++ src/views/Pools/components/CardFooter.tsx
@@ -103,7 +103,7 @@ const CardFooter: React.FC<Props> = ({
             <FlexFull>
               <Label>
                 <span role="img" aria-label="syrup">
-                  🥛{' '}
+                  🎖️{' '}
                 </span>
                 {TranslateString(408, 'Total')}
               </Label>
