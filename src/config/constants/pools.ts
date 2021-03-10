eeeb2640b7323988257331d24037b213d8e08681
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-10 07:25:49 -0500

    Remove TABLE from POOL...
    not sure if it's a good idea to toy with contracts.

--- src/config/constants/pools.ts
+++ src/config/constants/pools.ts
@@ -18,23 +18,6 @@ const pools: PoolConfig[] = [
     isFinished: false,
     tokenDecimals: 18,
   },
-  {
-    sousId: 5,
-    tokenName: 'KNIGHT',
-    stakingTokenName: QuoteToken.TABLE,
-    stakingTokenAddress: '0xf5a2f7418035ce76967f515f39d65719bb0453b6', // TABLE
-    contractAddress: {
-      97: '0x0E9b7E13a543b7F1651A6BB03a47900515eBceCf', // MasterChef
-      56: '0x0E9b7E13a543b7F1651A6BB03a47900515eBceCf', // MasterChef
-    },
-    poolCategory: PoolCategory.CORE,
-    projectLink: 'https://www.knightsdefi.com',
-    harvest: true,
-    tokenPerBlock: '1',
-    sortOrder: 2,
-    isFinished: false,
-    tokenDecimals: 18,
-  },
 ]
 
 export default pools
