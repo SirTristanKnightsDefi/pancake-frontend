4451b595f68e1aafc5d31f6e4e36ea5831d66bf8
Sir Tristan <sirtris@knightsdefi.com>
2021-04-23 07:17:23 -0400

    update for Pancake V2 migration (V1 links)

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -8,6 +8,20 @@ const config: MenuEntry[] = [
     href: '/',
   },
   {
+    label: "Trade",
+    icon: "TradeIcon",
+    items: [
+      {
+        label: "Exchange",
+        href: "https://v1exchange.pancakeswap.finance",
+      },
+      {
+        label: "Liquidity",
+        href: "https://v1exchange.pancakeswap.finance/#/pool",
+      },
+    ],
+  },
+  {
     label: 'Battlefield',
     icon: 'GroupsIcon',
     href: '/Battlefield',
@@ -38,72 +52,58 @@ const config: MenuEntry[] = [
     href: 'https://knightsoftheroundtable-bsc.medium.com/',
   },
   {
-    label: 'SQUIRE Info',
+    label: 'Buy Tokens',
     icon: 'InfoIcon',
     items: [
-      
+      {
+        label: 'Buy SQUIRE',
+        href: 'https://v1exchange.pancakeswap.finance/#/swap?outputCurrency=0x2F0D21f1B84F03fB9D60004fc206C86Be6902a32',
+      },
+      {
+        label: 'Buy KNIGHT',
+        href: 'https://v1exchange.pancakeswap.finance/#/swap?outputCurrency=0x16C0e0936E1B38Ff1F9b8a1e75d8ba29aDf87d30',
+      },  
+      {
+        label: 'Buy LEGEND',
+        href: 'https://v1exchange.pancakeswap.finance/#/swap?outputCurrency=0xDc661984735b535210CB1f52f86cc58616024192',
+      },
+      {
+        label: 'Buy TABLE',
+        href: 'https://v1exchange.pancakeswap.finance/#/swap?outputCurrency=0xf5a2f7418035ce76967f515f39d65719bb0453b6',
+      },
+    ],
+  },
+  {
+    label: 'Tokens Info',
+    icon: 'InfoIcon',
+    items: [
       {
         label: 'SQUIRE Contract',
         href: 'https://bscscan.com/address/0x2F0D21f1B84F03fB9D60004fc206C86Be6902a32',
       },
       {
-        label: 'Buy SQUIRE',
-        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x2F0D21f1B84F03fB9D60004fc206C86Be6902a32',
-      },
-      {
         label: 'SQUIRE Chart',
         href: 'https://dex.guru/token/0x2F0D21f1B84F03fB9D60004fc206C86Be6902a32',
       },
-    ]
-  },
-  {
-    label: 'KNIGHT Info',
-    icon: 'InfoIcon',
-    items: [
-      
       {
         label: 'KNIGHT Contract',
         href: 'https://bscscan.com/address/0x16c0e0936e1b38ff1f9b8a1e75d8ba29adf87d30',
       },
       {
-        label: 'Buy KNIGHT',
-        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0x16C0e0936E1B38Ff1F9b8a1e75d8ba29aDf87d30',
-      },
-      {
         label: 'KNIGHT Chart',
         href: 'https://dex.guru/token/0x16C0e0936E1B38Ff1F9b8a1e75d8ba29aDf87d30',
       },
-    ]
-  },  
-  {
-    label: 'LEGEND Info',
-    icon: 'InfoIcon',
-    items: [
       {
         label: 'LEGEND Contract',
         href: 'https://bscscan.com/address/0xDc661984735b535210CB1f52f86cc58616024192',
       },
       {
-        label: 'Buy LEGEND',
-        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xDc661984735b535210CB1f52f86cc58616024192',
-      },
-      {
         label: 'LEGEND Chart',
         href: 'https://dex.guru/token/0xDc661984735b535210CB1f52f86cc58616024192',
       },
-    ],
-  },
-  {
-    label: 'TABLE Info',
-    icon: 'InfoIcon',
-    items: [
       {
         label: 'TABLE Contract',
         href: 'https://bscscan.com/address/0xf5a2f7418035ce76967f515f39d65719bb0453b6',
-      },
-      {
-        label: 'Buy TABLE',
-        href: 'https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xf5a2f7418035ce76967f515f39d65719bb0453b6',
       },
       {
         label: 'TABLE Chart',
