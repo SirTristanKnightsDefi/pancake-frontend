5941b43551118dbe6ebc2ccba5cae0d429b82b70
Sir Tristan <sirtris@knightsdefi.com>
2021-06-11 08:09:42 -0400

    Update ApproveWbnbButton.tsx

--- src/components/ApproveWbnbButton.tsx
+++ src/components/ApproveWbnbButton.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import BigNumber from 'bignumber.js'
 import { Button, useWalletModal, Text, Flex } from '@pancakeswap-libs/uikit'
 import useI18n from 'hooks/useI18n'
+import { getBalanceNumber } from 'utils/formatBalance'
 import { useMilfNftWbnbPurchaseApprove, useMilfNftWbnbPurchaseApproveUnlimited } from 'hooks/useApprove'
 
 const ApproveWbnbButton = (amount) => {
@@ -12,10 +13,7 @@ const ApproveWbnbButton = (amount) => {
   return (
     <Flex>
       <Button height="128px" variant="secondary" mt="24px" mr="4px" onClick={onApprove}>
-              <Text>Approve WBNB - Price Only</Text>
-      </Button>
-      <Button height="128px" variant="secondary" mt="24px" onClick={onApproveUnlimited}>
-        <Text>Approve WBNB - Unlimited</Text>
+              <Text>Approve 0.5 WBNB</Text>
       </Button>
     </Flex>
   )
